3278818e44e2914ae4ee396ce544db5c91441094
Jarek Baran <git@jarekbaran.dev>
2023-03-27 14:44:26 +0200

    Refactor app styles and logic

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 let todos = [];
 let draggable = null;
 
-const todoList = document.querySelector('[data-js=show-todo-list]');
-const importBtn = document.querySelector('[data-js=import-todo-list]');
-const exportBtn = document.querySelector('[data-js=export-todo-list]');
-const newTodoBtn = document.querySelector('[data-js=add-new-todo]');
+const todoList = document.querySelector(`[data-js=show-todo-list]`);
+const importListBtn = document.querySelector(`[data-js=import-todo-list]`);
+const exportListBtn = document.querySelector(`[data-js=export-todo-list]`);
+const newTodoBtn = document.querySelector(`[data-js=add-new-todo]`);
 
 const autoHeight = (element) => {
 	element.style.height = `auto`;
@@ -15,7 +15,7 @@ const addNewTodo = () => {
 	const newTodo = {
 		id: new Date().getTime(),
 		complete: false,
-		text: '',
+		text: ``,
 	}
 
 	todos.unshift(newTodo);
@@ -24,90 +24,13 @@ const addNewTodo = () => {
 	
 	todoList.prepend(todo);
 	
-	text.removeAttribute('disabled');
+	text.removeAttribute(`disabled`);
 	text.focus();
 	
 	saveTodoList();
 }
 
-const createTodo = (newTodo) => {
-	const todo = document.createElement('li');
-	todo.classList.add('row', 'todo');
-	todo.setAttribute('data-js', 'drag-and-drop');
-
-	const handler = document.createElement('button');
-	handler.type = 'button';
-	handler.innerHTML = '&#8661;';
-	handler.setAttribute('draggable', 'true');
-	handler.setAttribute('data-id', newTodo.id);
-
-	const complete = document.createElement('input');
-	complete.type = 'checkbox';
-	complete.checked = newTodo.complete;
-
-	newTodo.complete && todo.classList.add('complete');
-
-	const text = document.createElement('textarea');
-	text.innerText = newTodo.text;
-	text.setAttribute('rows', '1');
-	text.setAttribute('disabled', '');
-
-	const actions = document.createElement('div');
-	actions.classList.add('actions');
-
-	const edit = document.createElement('button');
-	edit.innerHTML = '&#9998; Edit';
-
-	const remove = document.createElement('button');
-	remove.innerHTML = '&#10006; Remove';
-
-	actions.append(edit);
-	actions.append(remove);
-
-	todo.append(handler);
-	todo.append(complete);
-	todo.append(text);
-	todo.append(actions);
-
-	complete.addEventListener('change', () => {
-		newTodo.complete = complete.checked;
-		newTodo.complete ? todo.classList.add('complete') : todo.classList.remove('complete');
-
-		saveTodoList();
-	});
-
-	text.addEventListener('input', () => {
-		newTodo.text = text.value;
-
-		autoHeight(text);
-	});
-
-	text.addEventListener('blur', () => {
-		text.setAttribute('disabled', '');
-
-		!newTodo.text.length && todo.remove();
-
-		saveTodoList();
-	});
-
-	edit.addEventListener('click', () => {
-		text.removeAttribute('disabled');
-		text.focus();
-	});
-
-	remove.addEventListener('click', () => {
-		if (window.confirm(`Remove todo? - "${newTodo.text}"`)) {
-			todos = todos.filter(todo => todo.id != newTodo.id);
-			todo.remove();
-	
-			saveTodoList();
-		}
-	});
-
-	return {todo, text}
-}
-
-const moveTodo = (srcID, destID) => {
+const replaceTodo = (srcID, destID) => {
 	const srcIndex = todos.findIndex((todo) => todo.id == srcID);
 	const destIndex = todos.findIndex((todo) => todo.id == destID);
 
@@ -119,18 +42,89 @@ const moveTodo = (srcID, destID) => {
 	saveTodoList();
 }
 
+const removeTodo = (removeTodoId, createdTodo) => {
+	todos = todos.filter(todo => todo.id != removeTodoId);
+	createdTodo.remove();
+
+	saveTodoList();
+}
+
+const createTodo = (newTodo) => {
+	const todo = document.createElement(`li`);
+	todo.classList.add(`row`, `todo`);
+	todo.setAttribute(`data-js`, `drag-and-drop`);
+
+	const handler = document.createElement(`button`);
+	handler.type = `button`;
+	handler.innerHTML = `&#8661;`;
+	handler.setAttribute(`draggable`, `true`);
+	handler.setAttribute(`data-id`, newTodo.id);
+
+	const complete = document.createElement(`input`);
+	complete.type = `checkbox`;
+	complete.checked = newTodo.complete;
+
+	newTodo.complete && todo.classList.add(`complete`);
+
+	const text = document.createElement(`textarea`);
+	text.innerText = newTodo.text;
+	text.setAttribute(`rows`, `1`);
+	text.setAttribute(`disabled`, `true`);
+
+	const actions = document.createElement(`div`);
+	actions.classList.add(`actions`);
+
+	const edit = document.createElement(`button`);
+	edit.innerHTML = `&#9998; Edit`;
+
+	const remove = document.createElement(`button`);
+	remove.innerHTML = `&#10006; Remove`;
+
+	actions.append(edit, remove);
+
+	todo.append(handler, complete, text, actions);
+
+	complete.addEventListener(`change`, () => {
+		newTodo.complete = complete.checked;
+		newTodo.complete ? todo.classList.add(`complete`) : todo.classList.remove(`complete`);
+
+		saveTodoList();
+	});
+
+	text.addEventListener(`input`, () => {
+		newTodo.text = text.value;
+
+		autoHeight(text);
+	});
+
+	text.addEventListener(`blur`, () => {
+		text.setAttribute(`disabled`, `true`);
+
+		!text.value.length && removeTodo(newTodo.id, todo);
+	});
+
+	edit.addEventListener(`click`, () => {
+		text.removeAttribute(`disabled`);
+		text.focus();
+	});
+
+	remove.addEventListener(`click`, () => window.confirm(`Remove todo? - "${newTodo.text}"`) && removeTodo(newTodo.id, todo));
+
+	return {todo, text}
+}
+
 const loadTodoList = new Promise(function(resolve, reject) {
-	const loadedList = localStorage.getItem('todo-list');
+	const loadedList = localStorage.getItem(`todo-list`);
 
 	loadedList
 		? resolve(todos = JSON.parse(loadedList))
-		: reject('The todo-list in local storage not exists!');
+		: reject(`The todo-list in local storage not exists!`);
 });
 
-const saveTodoList = () => localStorage.setItem('todo-list', JSON.stringify(todos));
+const saveTodoList = () => localStorage.setItem(`todo-list`, JSON.stringify(todos));
 
 const displayTodoList = () => {
-	todoList.innerHTML = '';
+	todoList.innerHTML = ``;
 
 	todos.forEach(
 		listElement => {
@@ -142,16 +136,12 @@ const displayTodoList = () => {
 }
 
 const importTodoList = () => {
-	const inputFile = document.createElement('input');
+	const inputFile = document.createElement(`input`);
 
-	inputFile.type = 'file';
-	inputFile.accept = '.json';
-	
-	if (window.confirm('Are you sure? This will overwrite the current todo list!')) {
-		inputFile.click();
-	}
+	inputFile.type = `file`;
+	inputFile.accept = `.json`;
 
-	inputFile.addEventListener('change', () => {
+	inputFile.addEventListener(`change`, () => {
 		if (inputFile.value.length) {
 			const reader = new FileReader();
 
@@ -165,13 +155,15 @@ const importTodoList = () => {
 			}
 		}
 	});
+
+	window.confirm(`Are you sure? This will overwrite the current todo list!`) && inputFile.click();
 }
 
 const exportTodoList = () => {
-	if (!todos.length) return alert('Todo list is empty! Please add some todos before export.');
+	if (!todos.length) return alert(`Todo list is empty! Please add some todos before export.`);
 
-	const file = new Blob([JSON.stringify(todos)], {type: 'text/plain'});
-	const link = document.createElement('a');
+	const file = new Blob([JSON.stringify(todos)], {type: `text/plain`});
+	const link = document.createElement(`a`);
 
 	link.href = URL.createObjectURL(file);
 	link.download = `todo-list-${new Date().toJSON().slice(0,10).toString()}.json`;
@@ -183,10 +175,10 @@ const exportTodoList = () => {
 // Drag and drop handlers
 function handleDragStart(e) {
 	draggable = this;
-	draggable.classList.add('draggable');
+	draggable.classList.add(`draggable`);
 
-	e.dataTransfer.effectAllowed = 'move';
-	e.dataTransfer.setData('text/html', this.innerHTML);
+	e.dataTransfer.effectAllowed = `move`;
+	e.dataTransfer.setData(`text/html`, this.innerHTML);
 }
 
 function handleDragOver(e) {
@@ -194,14 +186,14 @@ function handleDragOver(e) {
 		e.preventDefault();
 	}
 
-	e.dataTransfer.dropEffect = 'move';
-	this.classList.add('over');
+	e.dataTransfer.dropEffect = `move`;
+	this.classList.add(`over`);
 	
 	return false;
 }
 
 function handleDragLeave() {
-	this.classList.remove('over');
+	this.classList.remove(`over`);
 }
 
 function handleDrop(e) {
@@ -209,14 +201,14 @@ function handleDrop(e) {
 		e.stopPropagation();
 	}
 
-	draggable.classList.remove('draggable');
-	this.classList.remove('over');
+	draggable.classList.remove(`draggable`);
+	this.classList.remove(`over`);
 	
 	if (draggable != this) {
 		draggable.innerHTML = this.innerHTML;
-		this.innerHTML = e.dataTransfer.getData('text/html');
+		this.innerHTML = e.dataTransfer.getData(`text/html`);
 
-		moveTodo(draggable.querySelector('[draggable=true]').dataset.id, this.querySelector('[draggable=true]').dataset.id);
+		replaceTodo(draggable.querySelector(`[draggable=true]`).dataset.id, this.querySelector(`[draggable=true]`).dataset.id);
 	}
 
 	return false;
@@ -224,22 +216,27 @@ function handleDrop(e) {
 
 // App init
 (function() {
-	loadTodoList.then(displayTodoList(), error => console.warn(error));
+	loadTodoList
+		.then(
+			displayTodoList(),
+			error => console.warn(error)
+		)
+		.then(() => {
+			window.addEventListener(`load`, () => {
+				importListBtn.addEventListener(`click`, importTodoList);
+				exportListBtn.addEventListener(`click`, exportTodoList);
+				newTodoBtn.addEventListener(`click`, addNewTodo);
 
-	window.addEventListener('load', () => {
-		importBtn.addEventListener('click', importTodoList);
-		exportBtn.addEventListener('click', exportTodoList);
-		newTodoBtn.addEventListener('click', addNewTodo);
+				document.querySelectorAll(`textarea`).forEach(textarea => {
+					autoHeight(textarea);
+				});
 
-		document.querySelectorAll('textarea').forEach(textarea => {
-			autoHeight(textarea);
+				document.querySelectorAll(`[data-js=drag-and-drop]`).forEach(function(dnd) {
+				dnd.addEventListener(`dragstart`, handleDragStart, false);
+				dnd.addEventListener(`dragover`, handleDragOver, false);
+				dnd.addEventListener(`dragleave`, handleDragLeave, false);
+				dnd.addEventListener(`drop`, handleDrop, false);
+				});
+			});
 		});
-
-		document.querySelectorAll('[data-js=drag-and-drop]').forEach(function(dnd) {
-		  dnd.addEventListener('dragstart', handleDragStart, false);
-		  dnd.addEventListener('dragover', handleDragOver, false);
-		  dnd.addEventListener('dragleave', handleDragLeave, false);
-		  dnd.addEventListener('drop', handleDrop, false);
-		});
-	});
 })();
